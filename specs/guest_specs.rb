927f71d37f2836a093a55f1c1bf4f82b75bb2134
John Taggart <taggartj1985@gmail.com>
2020-03-20 13:00:29 +0000

    basic customers added with money value

--- specs/guest_specs.rb
+++ specs/guest_specs.rb
@@ -1,3 +1,22 @@
 require("minitest/autorun")
 require('minitest/reporters')
+require_relative('../guest')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class GuestTest < MiniTest::Test
+
+  def setup
+    @guest1 = Guest.new("Slash", 400)
+    @guest2 = Guest.new("Mayer", 200)
+    @guest3 = Guest.new("Fruscainte", 560)
+  end
+
+  def test_customer_name
+    assert_equal("Slash", @guest1.name)
+  end
+
+  def test_customer_money
+    assert_equal(200, @guest2.money)
+  end
+
+end
